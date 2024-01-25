7b26fe4567ca269c2e72304022a85f7831e579c0
Julien Lemarchand <lemar.jul@gmail.com>
2024-01-25 12:44:52 +0100

    refactor(views): rename files, change paths ; style(css): update base.css

--- src/routes/app/index.js
+++ src/routes/app/index.js
@@ -6,24 +6,24 @@ const clearNotConfirmed = require("../../controllers/users/clear-not-confirmed.j
 const router = express
   .Router()
   .get("/", (req, res) => {
-    res.render("app");
+    res.render("home");
   })
+  .get("/about", (req, res) => res.render("about"))
   .get("/upcoming", (req, res) => res.render("upcoming"))
   .get("/dashboard", (req, res) => res.render("dashboard"))
+  .get("/profile", (req, res) => res.render("profile"))
+  .get("/discover", (req, res) => res.render("discover"))
   // Users
-  .get("/signup",users.signup)
+  .get("/signup", users.signup)
   .get("/login", users.login)
   .get("/clear-not-confirmed", clearNotConfirmed)
   .post("/signup", users.submit)
   .get("/training", users.training)
-  .get('/confirm-email', confirmEmail)
+  .get("/confirm-email", confirmEmail)
   // Flashcards
   .get("/flashcards/new", flashcards.new)
   .get("/flashcards/:id", flashcards.show)
   .post("/flashcards", flashcards.create)
   .delete("/flashcards/:id", flashcards.delete);
 
-  
-
 module.exports = router;
-
