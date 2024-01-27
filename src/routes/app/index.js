f86279b37196c17ae58e33fa8e5ece2f66dbb8cc
Julien Lemarchand <lemar.jul@gmail.com>
2024-01-27 11:03:47 +0100

    refactor(router;views):  move default testing value from router to discover.ejs; feat(router): add partials' router to app's routes

--- src/routes/app/index.js
+++ src/routes/app/index.js
@@ -3,6 +3,8 @@ const users = require("./users.js");
 const flashcards = require("./flashcards.js");
 const confirmEmail = require("../../controllers/users/confirm-email.js");
 const clearNotConfirmed = require("../../controllers/users/clear-not-confirmed.js");
+const loadPartialsRoutes = require("./partials-tester.js");
+
 const router = express
   .Router()
   .get("/", (req, res) => {
@@ -12,27 +14,7 @@ const router = express
   .get("/upcoming", (req, res) => res.render("pages/upcoming"))
   .get("/dashboard", (req, res) => res.render("pages/dashboard"))
   .get("/profile", (req, res) => res.render("pages/profile"))
-  .get("/discover", (req, res) =>
-    res.render("pages/discover", {
-      text : "Discover",
-      flashcards: [
-        {
-          id: 1,
-          question: "Flashcard 1",
-          answer: "Description 1",
-          image: "https://picsum.photos/200/300",
-          tags: ["tag1", "tag2", "tag3"],
-        },
-        {
-          id: 2,
-          question: "Flashcard 2",
-          answer: "Description 2",
-          image: "https://picsum.photos/200/300",
-          tags: ["tag1", "tag2", "tag3"],
-        },
-      ],
-    })
-  )
+  .get("/discover", (req, res) => res.render("pages/discover"))
   .get("/test/flashcard", (req, res) => res.render("partials/flashcard"))
   // Users
   .get("/signup", users.signup)
@@ -43,9 +25,16 @@ const router = express
   .get("/confirm-email", confirmEmail)
   .get("/forgot", users.views.forgotPassword)
   // Flashcards
+  .get("/flashcards", flashcards.views.list)
   .get("/flashcards/new", flashcards.views.new)
   .get("/flashcards/:id", flashcards.views.show)
   .post("/flashcards", flashcards.create)
   .delete("/flashcards/:id", flashcards.delete);
+  //partials testing
+  if (process.env.NODE_ENV === "development") {
+    loadPartialsRoutes(router);
+  }
+
+
 
 module.exports = router;
