b4049e7f0afd30c2743265db47e9d9c8a5202010
Julien Lemarchand <lemar.jul@gmail.com>
2024-01-29 08:50:38 +0100

    style(usersController): add/remove line breaks

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,9 +1,9 @@
 const userControllers = require("../../controllers/users");
-
 const router = require("express").Router();
 const auth = require("../../auth/auth.js");
 const checkAdminOrSelf = require("../../auth/check-admin-or-self.js");
 const sendConfirmationEmail = require("../../controllers/mail/send-email-confirmation.js");
+
 module.exports = (app, controller = userControllers) => {
   router.param("id", controller.findById);
   router.post("/login", controller.login);
