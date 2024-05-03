a6027e27ec204b96963be092bae4ca96881dacf0
Julien Lemarchand <lemar.jul@gmail.com>
2024-05-03 20:42:05 +0200

    docs: update JSDoc comments in userFlashCards-model.js

--- src/db/models/userFlashcards-model.js
+++ src/db/models/userFlashcards-model.js
@@ -1,17 +1,18 @@
-const { Model } = require("sequelize");
-
+// @ts-check
 /**
  * @typedef {import('sequelize').Sequelize} Sequelize
  * @typedef {import('sequelize').DataTypes} DataTypes
- * @typedef {import('sequelize').Model} Model
+ * @typedef {import('sequelize').Model} M
+ * @typedef {import('sequelize').ModelStatic<M>} ModelStatic
  */
 
 /**
- *  UserFlashcards Model
+ *  UserFlashcards Model - Represents the UserFlashcards table in the database.
  * @param {Sequelize} sequelize Sequelize Instance
  * @param {DataTypes} DataTypes DataTypes
- * @returns {Model} UserFlashcards Model
+ * @returns {ModelStatic} UserFlashcards Model
  */
+
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define("user_flashcard", {
     lastTraining: {
@@ -25,10 +26,15 @@ module.exports = (sequelize, DataTypes) => {
     progress: {
       type: DataTypes.INTEGER,
       defaultValue: 1,
-      max: 7,
-      min: 1,
+      validate: {
+        max: 7,
+        min: 1,
+      },
     },
   });
 };
 
+/* // ? Implement UserFlashcards model as a class ?
+const { Model } = require("sequelize");
 class UserFlashcards extends Model {}
+*/
