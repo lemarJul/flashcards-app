d88e15c9542713de7f8e9d567d80b82159715dc9
Julien Lemarchand <lemar.jul@gmail.com>
2024-05-04 10:46:18 +0200

    refactor: store the router in a variable befor epxorting

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const appRouter = require('./app');
-const apiRouter = require('./api');
+const appRouter = require("./app");
+const apiRouter = require("./api");
 
-console.log(__dirname + "/public");
-module.exports = express
+const router = express
   .Router()
-  .use(express.static("public"))
-  .use(appRouter)
-  .use("/api", apiRouter)
+  .use(express.static("public")) // serve static files from public folder
+  .use(appRouter) // use appRouter for all routes
+  .use("/api", apiRouter); // use apiRouter for all routes starting with /api
 
+module.exports = router;
