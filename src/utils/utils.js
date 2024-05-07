491635d8dc940c62036cc5f48ddabf049274ab05
Julien Lemarchand <lemar.jul@gmail.com>
2024-05-07 21:37:19 +0200

    refactor: change location of utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-module.exports.isDevENV = process.env.NODE_ENV === "development";
+
 
 module.exports.requireAllFilesInDirectory = function (directory) {
   const files = fs.readdirSync(directory);
