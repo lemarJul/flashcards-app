3edc1792dc1681e6934a6e2d2a19b4b47d174d89
Julien Lemarchand <lemar.jul@gmail.com>
2024-05-13 23:08:33 +0200

    feat: add a baseSevice

---
+++ src/services/base-service.js
@@ -0,0 +1,67 @@
+module.exports = (resourceName, Model) => {
+  console.log("resourceName", resourceName);
+  console.log("Model", Model);
+  function create(options) {
+    return Model.create(options).catch((error) => {
+      error.message = `Error in creating new ${resourceName}: ${error.message}`;
+      throw error;
+    });
+  }
+  function findById(id) {
+    return Model.findByPk(id).catch((error) => {
+      error.message = `Error in fetching requested ${resourceName} with id ${id}`;
+      throw error;
+    });
+  }
+  function findAll(options) {
+    return Model.findAll(options).catch((error) => {
+      error.message = `Error in fetching all ${resourceName}`;
+      throw error;
+    });
+  }
+  function findAndCountAll(options) {
+    return Model.findAndCountAll(options).catch((error) => {
+      error.message = `Error in fetching all ${resourceName}`;
+      throw error;
+    });
+  }
+  function updateById(id, options) {
+    return Model.update(options, {
+      where: {
+        id: id,
+      },
+    }).catch((error) => {
+      error.message = `Error in updating ${resourceName} with id ${id}`;
+      throw error;
+    });
+  }
+  function deleteById(id) {
+    return Model.destroy({
+      where: {
+        id: id,
+      },
+    }).catch((error) => {
+      error.message = `Error in deleting ${resourceName} with id ${id}`;
+      throw error;
+    });
+  }
+  function deleteAll() {
+    return Model.destroy({
+      where: {},
+      truncate: true,
+    }).catch((error) => {
+      error.message = `Error in deleting all ${resourceName}`;
+      throw error;
+    });
+  }
+
+  return {
+    create,
+    findById,
+    findAll,
+    findAndCountAll,
+    updateById,
+    deleteById,
+    deleteAll,
+  };
+};
