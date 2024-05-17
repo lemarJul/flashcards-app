752009e23eb1e59a6ca290a39edf61ef0604e625
Julien Lemarchand <lemar.jul@gmail.com>
2024-05-17 10:42:14 +0200

    refactor: implement flashcard controller with flashcard service

--- src/controllers/flashcards/index.js
+++ src/controllers/flashcards/index.js
@@ -1,22 +1,55 @@
-const BaseController = require("../base-controller/index.js");
-const { Flashcard } = require("../../db").sequelize.models;
-const resourceName = "flashcard";
-const baseUpdate = require("../base-controller/update.js");
-const updateModifier = require("./update-modifier.js");
+const service = require("../../services/flashcard.service.js");
+const controller = require("../base.controller.js")(service);
+const { parseHyphenatedString } = require("../../utils/utils.js");
 
 module.exports = {
-  create: require("../base-controller/create.js")(resourceName, Flashcard),
-  findAll: require("./find-all.js"),
-  updateById: baseUpdate(resourceName, Flashcard, updateModifier),
-  deleteById: require("../base-controller/delete-by-pk.js")(
-    resourceName,
-    Flashcard
-  ),
-  findById: require("../base-controller/find-by-pk.js")(
-    resourceName,
-    Flashcard
-  ),
+  create: controller.create,
+  findAll: [parseFinderOptions, controller.findAll],
+  updateById: controller.updateById,
+  deleteById: controller.deleteById,
+  findById: controller.findById,
   sendLoadedResource: require("../base-controller/send-loaded-resource.js")(
-    resourceName
+    service.name
   ),
 };
+
+//TODO: move to utils, and get rid of the require Model (orderClause)
+function parseFinderOptions(req, res, next) {
+  const defaultOrder = "category";
+  const { category, content, limit, offset, order } = req.query;
+
+  req.findOptions = {
+    where: whereClause(),
+    limit: parseInt(limit) || null,
+    offset: parseInt(offset) || null,
+    order: orderClause(),
+  };
+  next();
+
+  function whereClause() {
+    const output = {};
+
+    if (category) output.category = parseCategory();
+
+    if (content) {
+      output[Op.or] = [
+        { question: { [Op.like]: `%${content}%` } },
+        { answer: { [Op.like]: `%${content}%` } },
+      ];
+    }
+
+    return output;
+  }
+
+  function parseCategory() {
+    const arr = Array.isArray(category) ? category : category.split(","); // allow passing ?category=1&category=2 as well as ?category=1,2
+    return arr.map((category) => parseHyphenatedString(category));
+  }
+
+  function orderClause() {
+    
+    const { Flashcard } = require("../../db");
+    const orderExist = order && order in Flashcard.getAttributes();
+    return [orderExist ? order : defaultOrder];
+  }
+}
