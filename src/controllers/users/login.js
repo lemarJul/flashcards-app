bda9a67faf88f901d50eb57c4ee73e983cfdf2aa
Julien Lemarchand <lemar.jul@gmail.com>
2024-05-17 10:30:04 +0200

    refactor

--- src/controllers/users/login.js
+++ src/controllers/users/login.js
@@ -1,29 +1,29 @@
-const { User } = require("../../db/index.js").sequelize.models;
+const { User } = require("../../db/index.js");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { Api400Error, Api404Error } = require("../../errors/api-errors.js");
 const BaseError = require("../../errors/base-error.js");
 
 module.exports = async (req, res, next) => {
-
   try {
     const { username, password } = req.body;
+
     if (!username || !password) {
       throw new Api400Error("Please provide a username and a password");
     }
 
     const user = await User.findOne({
       where: {
-        username: req.body.username,
+        username,
       },
     });
 
     if (!user)
       throw new Api404Error(
-        `Requested user with username ${req.body.username} doesn't exist`
+        `Requested user with username ${username} doesn't exist`
       );
 
-    const isValid = await bcrypt.compare(req.body.password, user.password);
+    const isValid = await bcrypt.compare(password, user.password);
 
     if (!isValid) throw new Api400Error("Invalid password");
 
