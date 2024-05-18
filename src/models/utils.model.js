03ab38de12d28743ce42ce96ee4c75910b2b2370
Julien Lemarchand <lemar.jul@gmail.com>
2024-05-18 10:49:16 +0200

    docs: add JSDoc to noUpdate function

--- src/models/utils.model.js
+++ src/models/utils.model.js
@@ -1,3 +1,9 @@
+/**
+ *  Prevents the update of a field, except if it is null or undefined. useful as setter in a model definition
+ * @example set: noUpdate("id")
+ * @param {String} attName
+ * @returns
+ */
 function noUpdate(attName) {
   return function (val) {
     this.setDataValue(attName, this.getDataValue(attName) ?? val);
