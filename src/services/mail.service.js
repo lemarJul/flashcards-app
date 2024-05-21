e713553f964391432b92c946aaaf5d9f93d3a70b
Julien Lemarchand <lemar.jul@gmail.com>
2024-05-21 14:20:26 +0200

    feat: create sendConfirmationEmail in email service

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -25,4 +25,20 @@ async function sendAsAdmin({ to, subject, html }) {
   });
 }
 
-module.exports = { sendAsAdmin };
+async function sendConfirmationEmail({ username, email, confirmationToken }) {
+  const baseUrl = process.env.BASE_URL;
+
+  sendAsAdmin({
+    to: process.env.EMAIL_TEST || email,
+    subject: `Email Confirmation - ${process.env.APP_NAME}`,
+    html: `
+        <h1>Hello ${username}</h1>
+        <h2>Thank you for subscribing to ${process.env.APP_NAME} !</h2>. 
+        <p>Please confirm your email by clicking on the following <a href="${baseUrl}/confirm-email?token=${confirmationToken}">link</a>.</p>
+        <p>If your link has expired, please <a href="${baseUrl}/signup" >register</a> again.</p>
+        <p>See you soon!</p>
+        <p> - The ${process.env.APP_NAME} Team -</p>`,
+  });
+}
+
+module.exports = { sendAsAdmin, sendConfirmationEmail };
