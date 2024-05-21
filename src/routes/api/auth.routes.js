648c20a711957553edc533f9871cf90dfc9ae640
Julien Lemarchand <lemar.jul@gmail.com>
2024-05-21 18:09:36 +0200

    feat: add email confirmation route to auth routes

--- src/routes/api/auth.routes.js
+++ src/routes/api/auth.routes.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
   const route = "/auth";
   router.post(route + "/login", authController.login);
   router.post(route + "/register", authController.register);
-
+  router.get(route + "/confirm-email", authController.confirmUserEmail);
+  
   app.use(router);
-
 };
