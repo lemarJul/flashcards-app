980eba01c496f5f90827fe54172bf2c324325a73
Julien Lemarchand <lemar.jul@gmail.com>
2024-05-22 16:01:04 +0200

    chore: disable app routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const appRouter = require("./app");
+// const appRouter = require("./app");
 const apiRouter = require("./api");
 
 const router = express
   .Router()
   .use(express.static("public")) // serve static files from public folder
-  .use(appRouter) // use appRouter for all routes
+  // .use(appRouter) // use appRouter for all routes
   .use("/api", apiRouter); // use apiRouter for all routes starting with /api
 
 module.exports = router;
