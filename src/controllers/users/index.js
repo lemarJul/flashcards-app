2d4bb0a6e29fabffd8a3ec0fb3a9e17ff305a2b4
Julien Lemarchand <lemar.jul@gmail.com>
2024-05-24 11:35:38 +0200

    refactor(users.controller): compose methods with base controller methods

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -1,18 +1,10 @@
-const { User } = require("../../db");
-const resourceName = "user";
-const baseUpdate = require("../base-controller/update.js");
-
-
 const userService = require("../../services/user.service");
-//WIP: Implement the base controller
 const baseController = require("../base.controller.js")(userService);
+const resourceName = "user";
 
 module.exports = {
-  findById: require("../base-controller/find-by-pk.js")(resourceName, User),
-  updateById: baseUpdate(resourceName, User),
-  deleteById: require("../base-controller/delete-by-pk.js")(resourceName, User),
-  sendLoadedResource: require("../base-controller/send-loaded-resource.js")(
-    resourceName
-  ),
+  findById: baseController.findById,
+  updateById: baseController.updateById,
+  deleteById: baseController.deleteById,
   confirmEmail: require("./confirm-email.js"),
 };
