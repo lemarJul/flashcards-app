32adc2596ad265ab391d7d5d7e2316a60522d363
Julien Lemarchand <lemar.jul@gmail.com>
2024-05-24 11:27:34 +0200

    refactor(users.routes): change the  GET 'users/:id' handler

--- src/routes/api/users.routes.js
+++ src/routes/api/users.routes.js
@@ -5,13 +5,11 @@ const {
 const auth = require("../../middlewares/auth.middlewares.js");
 const router = require("express").Router();
 
-const basePath = "/users";
+router.all("*", auth.selfOrAdmin);
 router.param("id", attachUserToRequest);
-router
-  .route("/:id")
-  .all(auth.selfOrAdmin)
-  .get(userController.sendLoadedResource)
+router.route("/:id")
+  .get(userController.findById)
   .delete(userController.deleteById)
   .put(userController.updateById);
 
-module.exports = (app) => app.use(basePath, router);
+module.exports = (app) => app.use("/users", router);
