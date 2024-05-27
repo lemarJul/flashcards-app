db2640fcc59b23464739c7d8c693e3ff53e7eba6
Julien Lemarchand <lemar.jul@gmail.com>
2024-05-27 17:56:21 +0200

    feat(flashard.controller): add processStudiedFlashcards function

--- src/controllers/flashcards/index.js
+++ src/controllers/flashcards/index.js
@@ -1,6 +1,7 @@
 const service = require("../../services/flashcard.service.js");
 const controller = require("../base.controller.js")(service);
 const parseFinderOptions = require("./utils.controller.js");
+const { requireDefinedProps } = require("../utils.js");
 
 module.exports = {
   create: [includeUserIdToBody, controller.create],
@@ -8,9 +9,35 @@ module.exports = {
   updateById: controller.updateById,
   deleteById: controller.deleteById,
   findById: controller.findById,
+  processStudiedFlashcards,
 };
 
 function includeUserIdToBody(req, res, next) {
   req.body.userId = req.authUser.id;
   next();
+}
+
+async function processStudiedFlashcards(req, res, next) {
+  try {
+    const { flashcards: studiedFlashcards } = requireDefinedProps(req.body);
+
+    const output = await Promise.all(
+      studiedFlashcards.map(async ({ id, isSuccessful }) => {
+        try {
+          const flashcard = await service.findById(id);
+
+          return flashcard.study(isSuccessful);
+        } catch (err) {
+          next(err);
+        }
+      })
+    );
+
+    res.status(200).json({
+      message: "the study of flashcards has been taken into account",
+      data: output,
+    });
+  } catch (err) {
+    next(err);
+  }
 }
