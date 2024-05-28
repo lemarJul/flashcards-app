793c1c0a1f07b4c47248c37298da16fc832cf2cb
Julien Lemarchand <lemar.jul@gmail.com>
2024-05-28 13:15:04 +0200

    feat: add ping route

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 
+router.get("/ping", (req, res) => {
+  res.status(200).json({ message: "pong" });
+});
+
 router.get("/", (req, res) => {
   res.render("pages/api", { stack: ["Node.js", "Express", "Sequelize"] });
 });
