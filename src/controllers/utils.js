2dace1c4cabaab2aaa5ab6ddf11809a004b3f311
Julien Lemarchand <lemar.jul@gmail.com>
2024-05-28 13:02:57 +0200

    docs: add documentation for requireDefinedProps

--- src/controllers/utils.js
+++ src/controllers/utils.js
@@ -1,5 +1,12 @@
 const { Api400Error } = require("../errors/api-errors");
 
+/**
+ * Creates a proxy object that throws an error if a property is accessed but not defined.
+ *
+ * @param {object} obj - The object to create a proxy for.
+ * @returns {object} - The proxy object.
+ * @throws {Api400Error} - Throws an error if a property is accessed but not defined.
+ */
 function requireDefinedProps(obj) {
   return new Proxy(obj, {
     get(target, prop) {
