b7cf597c99b2555120025df9a58e1774ca7278ef
Julien Lemarchand <lemar.jul@gmail.com>
2024-12-13 11:04:46 +0100

    refactor: Use environment variables for database config
    * Use env vars for DB connection
    * Remove localConfig dependency
    * Simplify config structure
    * Improve logging
    * Use DB_URI if available

--- src/db/db.config.js
+++ src/db/db.config.js
@@ -3,30 +3,37 @@
  * @typedef {import('sequelize').Options} Options
  */
 
-const { isDevENV, localConfig } = require("../utils/development");
+const { isDevENV } = require("../utils/utils");
+const {
+  DB_DATABASE,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_DIALECT,
+  DB_PORT,
+  DB_URI,
+} = process.env;
+
+const URI =
+  DB_URI ||
+  `${DB_DIALECT}://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 
 /**
- * Database configuration
- * @type {[String, String, String, Options]}
- * @property {String} 0 - database
- * @property {String} 1 - username
- * @property {String} 2 - password
- * @property {Options} 3 - options
+ * Database configuration.  This module exports a configuration array suitable for use with Sequelize.
+ * The array contains the database URI and a set of options.  The URI is constructed from environment variables
+ * if DB_URI is not set. Logging is enabled in development environments.
+ * @type {[String, Options] }
+ * @property {String} URI
+ * @see https://sequelize.org/docs/v6/getting-started/#configuration
  */
-const config = isDevENV
-  ? localConfig
-  : [
-      "",
-      "",
-      "",
-      {
-        host: "",
-        dialect: "mariadb",
-        dialectOptions: {
-          timezone: "Etc/GMT-2",
-        },
-        logging: console.log,
-      },
-    ];
+const config = [
+  URI,
+  {
+    dialectOptions: {
+      timezone: "Etc/GMT-2",
+    },
+    logging: isDevENV ? console.log : false,
+  },
+];
 
 module.exports = config;
