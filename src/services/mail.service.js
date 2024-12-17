15159b09d00b23169ce1673ff210ab3193c58c88
Julien Lemarchand <lemar.jul@gmail.com>
2024-12-17 14:31:47 +0100

    fix: update confirmation email logic
    - The confirmation email now uses a dynamically generated validation URL passed as an argument instead of constructing it within the function.
    - This allows for greater flexibility and testability, as the URL generation can be handled externally.
    - Removed reliance on `BASE_URL` environment variable.
    - Improved code clarity by directly using `APP_NAME` environment variable.
    - Removed unnecessary resend registration link.

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -24,19 +24,19 @@ async function sendAsAdmin({ to, subject, html }) {
   });
 }
 
-async function sendConfirmationEmail({ username, email, confirmationToken }) {
-  const baseUrl = process.env.BASE_URL;
+async function sendConfirmationEmail({ username, email }, confirmationToken, validationUrl) {
+  const { APP_NAME } = process.env;
+  const confirmationLink = `${validationUrl}?token=${confirmationToken}`;
 
   sendAsAdmin({
     to: process.env.EMAIL_TEST || email,
     subject: `Email Confirmation - ${process.env.APP_NAME}`,
     html: `
         <h1>Hello ${username}</h1>
-        <h2>Thank you for subscribing to ${process.env.APP_NAME} !</h2>. 
-        <p>Please confirm your email by clicking on the following <a href="${baseUrl}/confirm-email?token=${confirmationToken}">link</a>.</p>
-        <p>If your link has expired, please <a href="${baseUrl}/signup" >register</a> again.</p>
+        <h2>Thank you for subscribing to ${APP_NAME} !</h2>. 
+        <p>Please confirm your email by clicking on the following <a href="${confirmationLink}">link</a>.</p>
         <p>See you soon!</p>
-        <p> - The ${process.env.APP_NAME} Team -</p>`,
+        <p> - The ${APP_NAME} Team -</p>`,
   });
 }
 
