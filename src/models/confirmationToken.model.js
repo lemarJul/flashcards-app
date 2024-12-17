d40f95210ec213c22f265f96e41ada19ffa564a8
Julien Lemarchand <lemar.jul@gmail.com>
2024-12-17 14:19:36 +0100

    feat: Implement confirmation token model
    - Created a `ConfirmationToken` model with `token` and `expiresAt` fields.
    - The `token` field is a primary key, generated as a random hex string.
    - The `expiresAt` field defaults to 1 hour from creation.
    - Associated the `ConfirmationToken` model with the `User` model via a foreign key `userId`.

---
+++ src/models/confirmationToken.model.js
@@ -0,0 +1,30 @@
+const crypto = require("crypto");
+
+module.exports = (sequelize, DataTypes) => {
+  // ConfirmationToken model
+  const ConfirmationToken = sequelize.define("ConfirmationToken", {
+    token: {
+      type: DataTypes.STRING,
+      primaryKey: true, // Use token as primary key
+      allowNull: false,
+      defaultValue: crypto.randomBytes(32).toString("hex"), // Generate a random token by default
+    },
+    expiresAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date(Date.now() + 3600 * 1000), // Token expires in 1 hour
+    },
+  });
+
+  ConfirmationToken.associate = function associate() {
+    const { User } = sequelize.models;
+    ConfirmationToken.belongsTo(User, {
+      foreignKey: {
+        name: "userId",
+        allowNull: false,
+      },
+    });
+  };
+
+  return ConfirmationToken;
+};
