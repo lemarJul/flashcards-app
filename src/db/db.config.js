53d02593b37d5c985bc634b6a45e32da1c6197c5
Julien Lemarchand <lemar.jul@gmail.com>
2024-12-17 14:08:06 +0100

    docs(db): Clarify database config type

--- src/db/db.config.js
+++ src/db/db.config.js
@@ -1,8 +1,4 @@
 // @ts-check
-/**
- * @typedef {import('sequelize').Options} Options
- */
-
 const { isDevENV } = require("../utils/utils");
 const {
   DB_DATABASE,
@@ -22,7 +18,7 @@ const URI =
  * Database configuration.  This module exports a configuration array suitable for use with Sequelize.
  * The array contains the database URI and a set of options.  The URI is constructed from environment variables
  * if DB_URI is not set. Logging is enabled in development environments.
- * @type {[String, Options] }
+ * @type {[String, import('sequelize').Options] }
  * @property {String} URI
  * @see https://sequelize.org/docs/v6/getting-started/#configuration
  */
