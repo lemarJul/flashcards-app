8c8a14ec63e2ab2cc018c5f3bddb5e9b32efad15
Julien Lemarchand <lemar.jul@gmail.com>
2025-01-05 12:37:12 +0100

    feat: Use config package for logging configuration
    Refactored the logging configuration to utilize the `config` package. This simplifies the configuration process and improves maintainability.
    
    - Replaced the previous conditional logging configuration with a single line using `config.get("logging")`.
    - Improved code readability and reduced complexity.

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
+// Project modules
+const { connectDB } = require("./src/db");
+const routes = require("./src/routes");
+const errorsHandler = require("./src/errors/errors-handler");
+const invalidPathHandler = require("./src/errors/invalid-path-handler");
+
 //node_modules
 const express = require("express");
 const bodyParser = require("body-parser");
 const favicon = require("serve-favicon");
 const cors = require("cors");
 const logger = require("morgan");
-const path = require("path");
-const fs = require("fs");
 const engine = require("ejs-mate");
+const config = require("config"); //Simplified config import
 
-// Project modules
-const { connectDB } = require("./src/db");
-const routes = require("./src/routes");
-const { isDevENV } = require("./src/utils/utils");
-const errorsHandler = require("./src/errors/errors-handler");
-const invalidPathHandler = require("./src/errors/invalid-path-handler");
-
-// Configuration
-const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, "logs", "access.log"),
-  { flags: "a" }
-);
-const logConfig = isDevENV
-  ? ["dev"]
-  : ["combined", { stream: accessLogStream }];
 
 connectDB();
 
@@ -32,7 +22,7 @@ module.exports = class MnemoniacApp extends express {
     this.engine("ejs", engine);
     this.set("view engine", "ejs");
     this.set("views", __dirname + "/views");
-    this.use(logger(...logConfig));
+    this.use(logger(...config.get("logging"))); //Simplified logger config
     this.use(favicon(__dirname + "/public/images/card.ico"));
     this.use(bodyParser.urlencoded({ extended: false }));
     this.use(bodyParser.json());
