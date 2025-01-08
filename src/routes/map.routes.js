4a204fcf3bfa56acf85399948f4e0ea12777471f
Julien Lemarchand <lemar.jul@gmail.com>
2025-01-08 19:12:51 +0100

    feat(routes): Refactor map.routes.js for better organization and maintainability
    This commit refactors `map.routes.js` to improve code organization and maintainability.  Key changes include:
    
    - Separated controllers and middlewares into distinct `c` and `mw` objects for better readability and modularity.
    - Replaced direct controller and middleware imports with named imports.
    - Used a `config` module to retrieve the app version.
    - Improved the structure of the route definitions for clarity and ease of maintenance.
    - Added rate limiters for login and refresh token endpoints.

--- src/routes/map.routes.js
+++ src/routes/map.routes.js
@@ -1,80 +1,91 @@
 // controllers & middlewares
-const config = require("../../app.config");
+const config = require("config");
 const { parseSemVer } = require("../utils/utils");
-const Auth = require("../controllers/auth.controller");
-const Flashcard = require("../controllers/flashcards/flashcards.controller");
-const User = require("../controllers/users");
-const Category = require("../controllers/categories");
-const mwAuth = require("../middlewares/auth.middlewares");
-const { attachFlashcardToRequest } = require("../middlewares/flashcards.middlewares");
-const { attachUserToRequest } = require("../middlewares/users.middlewares");
-
-// Schemas
-const validate = require("../middlewares/validate.middleware");
+const apiVersion = parseSemVer(config.get("app.version"))?.major || 1;
 const Schemas = require("../schemas");
 
-const version = parseSemVer(config.app.version)?.major;
-
-const PONG = (req, res) => {
-  res.json({ message: `pong${req.ping ? " with param " + req.ping : ""}` });
+const c = {
+  pong: (req, res) => {
+    res.json({ message: `pong${req.ping ? " with param " + req.ping : ""}` });
+  },
+  Auth: require("../controllers/auth.controller"),
+  Flashcard: require("../controllers/flashcards/flashcards.controller"),
+  User: require("../controllers/users"),
+  Category: require("../controllers/categories"),
 };
-const PING_PARAM = (req, res, next, id) => {
-  req.ping = id;
-  next();
+
+const mw = {
+  pongParams: (req, res, next, id) => {
+    req.ping = id;
+    next();
+  },
+  Auth: require("../middlewares/auth.middlewares"),
+  FlashCard: require("../middlewares/flashcards.middlewares"),
+  User: require("../middlewares/users.middlewares"),
+  validateBody: require("../middlewares/validate.middleware"),
 };
 
 module.exports = {
-  // params
-  ping: { param: PING_PARAM },
-  [Flashcard.name]: { param: attachFlashcardToRequest },
-  [User.name]: { param: attachUserToRequest },
+  //* params
+  ping: { param: mw.pongParams },
+  [c.Flashcard.name]: {
+    param: mw.FlashCard.attachFlashcardToRequest,
+  },
+  [c.User.name]: { param: mw.User.attachUserToRequest },
 
-  // routes
+  //* routes
   "/api": {
-    ["/v" + version]: {
+    ["/v" + apiVersion]: {
       "/ping": {
-        get: PONG,
+        get: c.pong,
         "/:ping": {
-          get: PONG,
+          get: c.pong,
         },
       },
       "/auth": {
         "/confirm-email/:token": {
-          get: Auth.confirmUserEmail,
+          get: c.Auth.confirmUserEmail,
         },
         "/login": {
-          post: [validate(Schemas.Auth.login), Auth.login],
+          post: [
+            mw.Auth.limiters.loginLimiter,
+            mw.validateBody(Schemas.Auth.login),
+            c.Auth.login,
+          ],
         },
         "/register": {
-          post: [validate(Schemas.Auth.register), Auth.register],
+          post: [mw.validateBody(Schemas.Auth.register), c.Auth.register],
         },
         "/logout": {
-          post: [mwAuth.user, Auth.logout],
+          post: [mw.Auth.user, c.Auth.logout],
+        },
+        "/refresh": {
+          post: [mw.Auth.limiters.refreshTokenLimiter, c.Auth.refresh],
         },
       },
       "/*": {
-        all: mwAuth.user,
+        all: mw.Auth.user,
       },
       "/flashcards": {
-        get: Flashcard.findAll,
-        post: Flashcard.create,
+        get: c.Flashcard.findAll,
+        post: c.Flashcard.create,
         "/:flashcard": {
-          get: Flashcard.findById,
-          put: Flashcard.updateById,
-          delete: Flashcard.deleteById,
+          get: c.Flashcard.findById,
+          put: c.Flashcard.updateById,
+          delete: c.Flashcard.deleteById,
         },
         "/review": {
-          post: Flashcard.processFlashcardReviews,
+          post: c.Flashcard.processFlashcardReviews,
         },
       },
       "/categories": {
-        get: Category.findAll,
+        get: c.Category.findAll,
       },
       "/users": {
         "/:user": {
-          get: User.findById,
-          put: User.updateById,
-          delete: User.deleteById,
+          get: c.User.findById,
+          put: c.User.updateById,
+          delete: c.User.deleteById,
         },
       },
     },
